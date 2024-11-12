c8be0fb5fde788d531d6823d2a0a33bad2db8310
YukiKawashima <k0906yuki@gmail.com>
2024-11-12 21:10:13 +0900

    APIを使うように変更

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,11 +1,49 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { generateClient } from 'aws-amplify/api';
+import { listTodos } from '@/graphql/queries';
+import { Amplify } from 'aws-amplify';
+import awsExports from '@/aws-exports'; // aws-exports.jsのパスを確認
+
+Amplify.configure(awsExports);
+
 export default function Home() {
-    return (
-      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-          <h1 className="text-4xl font-bold text-center sm:text-left">This is list page!</h1>
-        </main>
-        <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        </footer>
-      </div>
-    );
-  }
+  const client = generateClient();
+  const [todos, setTodos] = useState<{ id: string; name: string; description: string }[]>([]);
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      try {
+        const result = await client.graphql({ query: listTodos });
+        console.log(result);
+        if (result.data.listTodos) {
+          const list = result.data.listTodos.items as Array<{ id: string; name: string; description: string }>;
+          setTodos(list);
+        }
+      } catch (error) {
+        console.error('Error fetching todos:', error);
+      }
+    };
+
+    fetchTodos();
+  }, []);
+
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <h1 className="text-4xl font-bold text-center sm:text-left">This is list page!</h1>
+        <ul>
+          {todos.map(todo => (
+            <li key={todo.id}>
+              <h2>{todo.name}</h2>
+              <p>{todo.description}</p>
+            </li>
+          ))}
+        </ul>
+      </main>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+      </footer>
+    </div>
+  );
+}
